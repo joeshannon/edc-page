21ae559ec1c4c22fbb57b980a111f3999ba01d2b
Joe Shannon <35498448+joeshannon@users.noreply.github.com>
2020-03-10 14:37:58 +0000

    Ignore image alt attributes
    At least for now.

--- cibuild.sh
+++ cibuild.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 bundle exec jekyll build
-bundle exec htmlproofer --allow-hash-href --url-swap "www.exeterdramacompany.co.uk:" ./_site
+bundle exec htmlproofer --allow-hash-href --url-swap "www.exeterdramacompany.co.uk:" --alt-ignore '/.*/' ./_site
