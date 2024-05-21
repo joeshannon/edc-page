38437968fd8283e4e1f51e01bcfd11ebfa8b6d31
Joe Shannon <35498448+joeshannon@users.noreply.github.com>
2024-05-21 15:03:55 +0100

    Remove old CI script

--- cibuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e # halt script on error
-
-bundle exec jekyll build
-bundle exec htmlproofer --allow-hash-href --url-swap "https\\://www.exeterdramacompany.co.uk:" --alt-ignore '/.*/' ./_site
